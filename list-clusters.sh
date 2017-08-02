9631a3279527155448695aea1399e22c9ed59d1a
Tom Meyer <meyerto@gmail.com>
2017-08-02 09:43:44 -0700

    Improvements to list-clusters.sh
    - tab delimiting for column command
    - print pipeline url

--- list-clusters.sh
+++ list-clusters.sh
@@ -5,13 +5,14 @@ function list_running_terraform_instances() {
 }
 
 function extract_name_and_ip_and_launch_time() {
-    jq -r '[.Reservations[] | .Instances[] | {Time: .LaunchTime, NodeIp: .PrivateIpAddress, NodeName: .Tags[] | select(.Key=="Name") | .Value}] | sort_by(.NodeName) | .[] | "\(.NodeIp) \(.NodeName) \(.Time)"'
+    jq -r '[.Reservations[] | .Instances[] | {Time: .LaunchTime, NodeIp: .PrivateIpAddress, NodeName: .Tags[] | select(.Key=="Name") | .Value, URL: .Tags[] | select(.Key=="PipelineUrl") | .Value}] | sort_by(.URL) | .[] | "\(.NodeIp) \(.NodeName) \(.Time) \(.URL)"'
 }
 
 function print_table_with_pacific_launch_time() {
-    while read -r node_ip node_name time ; do
-        echo "$node_ip" "$node_name" "$(TZ='America/Los_Angeles' date --date="$time" +'%Y-%m-%d %H:%M:%S %Z')"
-    done | column -t
+    while read -r node_ip node_name time url ; do
+        cali_date="$(TZ='America/Los_Angeles' date --date="$time" +'%Y-%m-%d %H:%M:%S %Z')"
+        echo -e "$node_name \t $node_ip \t $cali_date \t $url"
+    done | column -t -s $'\t'
 }
 
 list_running_terraform_instances | extract_name_and_ip_and_launch_time | print_table_with_pacific_launch_time
